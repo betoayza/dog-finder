33af0039d61ce122be222f8a0dfa3dae157f84d4
Beto <aaayza@gmail.com>
2021-10-23 21:02:26 -0300

    Cambios tercer dia parte2

--- data/manejarBusqueda.js
+++ data/manejarBusqueda.js
@@ -2,29 +2,33 @@
 //(solo van las definiciones de las funciones
 //La invocacion va en index.js)
 
+import fetch from "node-fetch";
+
 const handleFetch = async url => {
+   //fetch ya de por si retorna una Promesa
    const res = fetch(url);
+   //Se maneja la Promesa en caso de errores
    return await handleError(res);
 }
 
 const handleError = response => {
-   if(!response.ok){
+   if (!response.ok) {
       throw new Error(response.statusText);
    }
    return response;
 }
 
+//Retorna una Promesa que será manejada desde la invocación
 const buscarRazaPorNombre = nombreRaza => {
-   return new Promise( (resolve, reject) => {
-      const url = `https://dog.ceo/api/breed/${nombreRaza}/images/random`;
-      handleFetch(url)
-            .then(res => { console.log(res); resolve(res) })
-            .catch(error => { reject (error); })         
-   })
+   const url = `https://dog.ceo/api/breed/${nombreRaza}/images/random`;
+   //Se devuelve el manjejo de Fetch
+   return handleFetch(url)
+            .then(res => {
+               res.json();
+            })
+            .catch(error => {
+               error.json();
+            })
 };
 
-module.exports = { buscarRazaPorNombre }; 
-   
-
-
-
+export default buscarRazaPorNombre;
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require ('express');
-const hbs = require('hbs');
+import express from 'express';
+import hbs from 'hbs';
 const app = express();
-const { buscarRazaPorNombre } = require(`${__dirname}/data/manejarBusqueda`);
+import buscarRazaPorNombre from './data/manejarBusqueda.js';
 
 // Establece motor de plantillas
 app.set('view engine', 'hbs');
 // Registrar dónde están las vistas parciales
-hbs.registerPartials(`${__dirname}/views`, error => {console.log(error)});
+hbs.registerPartials('./views', error => {
+    console.log(error)
+});
 
 //CONFIGURACIONES
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 
 
 //MIDDLEWARE
@@ -17,6 +19,7 @@ app.set('port', process.env.PORT || 3000);
 //app.use(require('./rutas/rutas'));
 // Habilita la carpeta public para servir contenido estático
 //app.use(express.static(`${__dirname}/public`));
+
 // Middleware para parsear el cuerpo de las peticiones HTTP
 app.use(express.urlencoded({
     extended: true
@@ -26,39 +29,45 @@ app.use(express.urlencoded({
 //RUTAS PARA EL CLIENTE (NAVEGADOR)
 app.get('/', (req, res) => {
     //res.send('Pagina Principal');
-    res.render('home', { nombrePagina: 'BuscadorDePerrosOnline'});
-});   
+    res.render('home', {
+        nombrePagina: 'BuscadorDePerrosOnline'
+    });
+});
 
 app.get('/catalogoRazas', (req, res) => {
     //res.send('Pagina Principal');
-    res.render('catalogoRazas', { nombrePagina: 'CatalogoRazas'});
-}); 
+    res.render('catalogoRazas', {
+        nombrePagina: 'CatalogoRazas'
+    });
+});
 
-app.get('/resultadoBusqueda', (req, res) => {
-    //res.send('Pagina Principal');
-    res.render('resultadoBusqueda', { nombrePagina: 'ResultadoBusqueda'});
-}); 
+//mostrar 'home' para peticion GET
+app.get('/home', (req, res) => { //en post vale el res.send()     
+    res.render('home');
+});
 
+//respuesta al cliente mediante peticion get
+app.get('/resultadoBusqueda', async (req, res) => {
+    try {
+        const {
+            raza //debe coincidir con el name del input
+        } = req.body; //recibe la solicitd GET y extrae el nombreRaza
+        //Devuelve una Promesa
+        const respuesta = await buscarRazaPorNombre(raza);   
+        //muestra la pagina con la respuesta 
+        res.render('resultadoBusqueda', { respuesta });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+//Rutas por default para cualquier peticion GET
 app.get('*', (req, res) => {
     //muestra plantilla 404 en las rutas no especificadas
     //res.render('404');
     res.render('error404');
 });
 
-//RUTAS PARA EL SERVIDOR
-//petition post para agregar recurso al servidor
-app.post('/home', async (req, res) => {    //en post vale el res.send()     
-    try {        
-        const {
-            raza, //debe coincidir con el name del input
-        } = req.body; 
-        const resultado = await buscarRazaPorNombre(raza);
-        console.log(resultado);
-        //res.send(resultado);        
-    } catch (error) {
-        res.send(error);
-    }
-});
 
 //SERVIDOR
 app.listen(3000, () => {
